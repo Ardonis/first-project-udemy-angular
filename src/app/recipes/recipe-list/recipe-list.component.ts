84ee980b57cbfbdd0e53f8d1b335dd28a54334e1
Ardonis <vitiaardonis@gmail.com>
2023-02-23 09:50:42 +0100

    recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,15 +7,23 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'Recipe Image', 'https://picturetherecipe.com/wp-content/uploads/2020/04/PictureTheRecipe-Butter-Chicken.jpg' ),
-    new Recipe('A test Recipe', 'Recipe Image', 'https://picturetherecipe.com/wp-content/uploads/2020/04/PictureTheRecipe-Butter-Chicken.jpg' )
+    new Recipe(
+      'A test Recipe',
+      'Recipe Image',
+      'https://img.freepik.com/free-photo/vegetables-set-left-black-slate_1220-685.jpg?w=1060&t=st=1677141779~exp=1677142379~hmac=261aa9d96ac0383c5dac93d195f2d9bc4f9fb4ee4547390f18dac96650f6d330'
+    ),
+    new Recipe(
+      'A test Recipe',
+      'Recipe Image',
+      'https://picturetherecipe.com/wp-content/uploads/2020/04/PictureTheRecipe-Butter-Chicken.jpg'
+    ),
   ];
 
-  constructor() {
-
-  }
-  ngOnInit() {
-
+  constructor() {}
+  ngOnInit() {}
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 }
