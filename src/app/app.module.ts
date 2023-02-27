16f42646496aaf1da2e19a8042471329e941444f
Ardonis <vitiaardonis@gmail.com>
2023-02-27 09:39:01 +0100

    add new items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,6 @@ import { FormsModule } from '@angular/forms';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    FormsModule ,
   ],
   imports: [
     BrowserModule
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shred/ingredient.module';
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent {
+@ViewChild('nameInput', {static: false}) nameInputRef:ElementRef;
+@ViewChild('amountInput', {static: false}) amountInputRef:ElementRef;
+IngridinetAdded =new EventEmitter<Ingredient>();
+@Output() ingridinetAdded =new EventEmitter<Ingredient>();
 
+  onAddItem(){
+    const ingName =this.nameInputRef.nativeElement.value;
+    const ingamount =this.amountInputRef.nativeElement.value;
+    const newIngredient =new Ingredient(ingName, ingamount)
+    this.ingridinetAdded.emit(newIngredient)
+  }
 }
